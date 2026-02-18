0d1d4518fc55a78a8be1352d8ef79e3f3c9cf8a2
ADITYAECE36 <adityakumarswai123@gmail.com>
2026-02-18 11:12:33 +0530

    Update sever.py

--- sever.py
+++ sever.py
@@ -2,34 +2,26 @@ from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
-locations = {}
+latest_data = {}
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "GPS Server Running"
-
-@app.route("/map")
-def map_page():
     return render_template("map.html")
 
-@app.route("/update", methods=["POST"])
+
+@app.route('/update', methods=['POST'])
 def update():
-    data = request.json
-
-    device = data.get("device")
-    lat = data.get("lat")
-    lon = data.get("lon")
-
-    if device:
-        locations[device] = {"lat": lat, "lon": lon}
-
+    global latest_data
+    latest_data = request.json
+    print("Received:", latest_data)
     return {"status": "ok"}
 
-@app.route("/location")
+
+@app.route('/location')
 def location():
-    return jsonify(locations)
+    return jsonify(latest_data)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
 
